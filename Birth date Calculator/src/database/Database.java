bf6119ddaa213ef3fc41287d3149ff72d730fc0c
Nachiket Kamod <47713768+nachikamod@users.noreply.github.com>
2019-06-03 21:27:37 +0530

    Splitting
    Year Splitting is improved. Tome complexity is reduced by removing multiple transitions by logical splitting.

--- Birth date Calculator/src/database/Database.java
+++ Birth date Calculator/src/database/Database.java
@@ -3,18 +3,11 @@ package database;
 import swap.Swapper;
 
 public class Database {
-	public char[] first_two_digts;
-	public char[] last_two_digits;
 	public Database(int day, int month, String year){
 		
-		StringBuffer sb = new StringBuffer(year);
-		first_two_digts = new char[2];
-		sb.getChars(0, 2, first_two_digts, 0);
-		int c = Integer.parseInt(new String(first_two_digts));
-	
-		last_two_digits = new char[2];
-		sb.getChars(2, 4, last_two_digits, 0);
-		int d = Integer.parseInt(new String(last_two_digits));
+		int c = year / 100;
+		
+		int d = year - (c * 100);
 		
 		int k = day;
 		Swapper sw = new Swapper(k, month, c, d);
